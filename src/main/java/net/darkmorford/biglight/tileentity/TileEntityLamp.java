e37f48e64a7da7b5238c9ed0cdc6c39a3eebb328
DarkMorford <DarkMorford@users.noreply.github.com>
2017-09-28 09:39:40 -0700

    Allow TileEntityLamp to place light blocks as well.

--- src/main/java/net/darkmorford/biglight/tileentity/TileEntityLamp.java
+++ src/main/java/net/darkmorford/biglight/tileentity/TileEntityLamp.java
@@ -5,26 +5,76 @@ import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ITickable;
 import net.minecraft.util.math.BlockPos;
 
-public class TileEntityLamp extends TileEntity
+public class TileEntityLamp extends TileEntity implements ITickable
 {
 	private static final Block LIGHT_BLOCK      = Blocks.GLOWSTONE;
 	private static final int   LIGHT_BLOCK_META = 0;
 
-	public void removeLightBlocks()
+	@Override
+	public void update()
 	{
 		int radius = GeneralConfig.searchRadius;
 
-		for (int posX = -radius; posX < radius; ++posX)
+		// Select a random block inside our range
+		int offsetX = world.rand.nextInt(radius);
+		int offsetY = world.rand.nextInt(radius);
+		int offsetZ = world.rand.nextInt(radius);
+
+		if (world.rand.nextBoolean())
 		{
-			for (int posY = 0; posY < radius; ++posY)
+			offsetX *= -1;
+		}
+
+		if (world.rand.nextBoolean())
+		{
+			offsetZ *= -1;
+		}
+
+		int posX = pos.getX() + offsetX;
+		int posY = pos.getY() - offsetY;
+		int posZ = pos.getZ() + offsetZ;
+
+		// Don't place lights too close to bedrock
+		if (posY < 5)
+		{
+			posY = 5;
+		}
+
+		// Try to put down a light source
+		tryPlaceLightBlock(posX, posY, posZ);
+	}
+
+	public void removeLightBlocks()
+	{
+		int radius = GeneralConfig.searchRadius - 1;
+
+		for (int posX = -radius; posX <= radius; ++posX)
+		{
+			for (int posY = -radius; posY <= 0; ++posY)
 			{
-				for (int posZ = -radius; posZ < radius; ++posZ)
+				for (int posZ = -radius; posZ <= radius; ++posZ)
 				{
 					tryRemoveLightBlock(pos.add(posX, posY, posZ));
 				}
 			}
+		}
+	}
+
+	private void tryPlaceLightBlock(int x, int y, int z)
+	{
+		tryPlaceLightBlock(new BlockPos(x, y, z));
+	}
+
+	private void tryPlaceLightBlock(BlockPos pos)
+	{
+		IBlockState blockState = world.getBlockState(pos);
+
+		if (world.isAirBlock(pos) && blockState.getBlock() != LIGHT_BLOCK && world.getLight(pos) < GeneralConfig.maximumLight)
+		{
+			world.setBlockState(pos, LIGHT_BLOCK.getStateFromMeta(LIGHT_BLOCK_META), 2 | 16);
 		}
 	}
 
